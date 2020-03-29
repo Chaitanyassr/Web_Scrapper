7767b429004bbc939cb979917e90f6039dd1193e
CHAITANYA <Chaitanyas0101@gmail.com>
2020-03-29 16:01:07 +0530

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -1,3 +1,6 @@
+from bs4 import BeautifulSoup as Soup
+from urllib.request import urlopen as uReq
+
 my_url = 'https://www.worldometers.info/coronavirus/country/india/'
 
 uClient = uReq(my_url)
@@ -5,4 +8,10 @@ page_html = uClinet.read()
 uClinet.close()
 page_soup = soup(page_html, "html.parser")
 
+'''where in below code we deifne a variable which acutlly represnet the container class of the page that contain information'''
 con = page_soup.findAll("div", ("class": "container"))
+
+cont = con[0]
+print(con.div.)
+
+
